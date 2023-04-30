456a3e5fd396934873ba9d2b303bc678184c8bd8
Ulvi <ulvi.ehmedli2001@gmail.comm>
2023-04-30 00:43:28 -0700

    #18 Solution for

---
+++ src/main/java/MaxCalculator.java
@@ -0,0 +1,11 @@
+public class MaxCalculator {
+    
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    
+}
