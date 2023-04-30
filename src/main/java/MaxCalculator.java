1eeadedb1bfad437dab655fea255db300d0b6fd4
Ulvi <ulvi.ehmedli2001@gmail.comm>
2023-04-30 15:51:38 -0700

    #18 editing the max function

--- src/main/java/MaxCalculator.java
+++ src/main/java/MaxCalculator.java
@@ -1,7 +1,7 @@
 public class MaxCalculator {
     
     public static int max(int a, int b) {
-        if (a > b) {
+        if (a >= b) {
             return a;
         } else {
             return b;
