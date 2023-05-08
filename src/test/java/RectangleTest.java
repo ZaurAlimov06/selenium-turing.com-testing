a417f3482f55d469858f41516be1d3dee79545b1
Ulvi <ulvi.ehmedli2001@gmail.comm>
2023-05-07 18:14:57 -0700

    #104 Adding main function test

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class RectangleTest {
@@ -6,71 +7,53 @@ public class RectangleTest {
     Rectangle rectangle;
 
     @Before
-    public void setUp() throws RectangleException {
-        rectangle = new Rectangle(4.0, 6.0);
+    public void testRectangleCreation() throws RectangleException {
+        rectangle = new Rectangle(6.0, 8.0);
+        Assert.assertEquals(6.0, rectangle.getA(), 0.0);
+        Assert.assertEquals(8.0, rectangle.getB(), 0.0);
     }
 
     @Test
-    public void testGetArea() {
-        assertEquals(24.0, rectangle.getArea(), 0.001);
+    public void testInvalidRectangleCreation() {
+        assertThrows(RectangleException.class, () -> new Rectangle(-6.0, 8.0));
+    }
+
+    @Test(expected = RectangleException.class)
+    public void testAInvalid() throws RectangleException {
+        Rectangle rect = new Rectangle(-3.0, 8.0);
+    }
+
+    @Test(expected = RectangleException.class)
+    public void testBInvalid() throws RectangleException {
+        Rectangle rect = new Rectangle(3.0, -8.0);
     }
 
     @Test
-    public void testGetPerimeter() {
-        assertEquals(20.0, rectangle.getPerimeter(), 0.001);
+    public void getAreaTest() {
+        assertEquals(48.0, rectangle.getArea(), 0.0);
     }
 
     @Test
-    public void testSetA() {
-        rectangle.setA(8.0);
-        assertEquals(8.0, rectangle.getA(), 0.001);
-        assertEquals(48.0, rectangle.getArea(), 0.001);
-        assertEquals(28.0, rectangle.getPerimeter(), 0.001);
+    public void getPerimeterTest() {
+        assertEquals(28.0, rectangle.getPerimeter(), 0.0);
     }
 
     @Test
-    public void testSetB() {
-        rectangle.setB(10.0);
-        assertEquals(10.0, rectangle.getB(), 0.001);
-        assertEquals(40.0, rectangle.getArea(), 0.001);
-        assertEquals(28.0, rectangle.getPerimeter(), 0.001);
-    }
-
-    @Test
-    public void testIsSquare() {
+    public void checkSquareTests() {
         assertFalse(rectangle.isSquare());
-        rectangle.setA(5.0);
-        rectangle.setB(5.0);
+
+        rectangle.setA(4.0);
+        rectangle.setB(4.0);
         assertTrue(rectangle.isSquare());
     }
 
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithNegativeA() throws RectangleException {
-        Rectangle rect = new Rectangle(-2.0, 6.0);
-    }
-
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithNegativeB() throws RectangleException {
-        Rectangle rect = new Rectangle(8.0, -12.0);
-    }
-
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithNegativeBoth() throws RectangleException {
-        Rectangle rect = new Rectangle(-5.0, -3.0);
-    }
-
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithZeroA() throws RectangleException {
-        Rectangle rect = new Rectangle(0.0, 6.0);
-    }
-
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithZeroB() throws RectangleException {
-        Rectangle rect = new Rectangle(8.0, 0.0);
-    }
-
-    @Test(expected = RectangleException.class)
-    public void testConstructorWithZeroBoth() throws RectangleException {
-        Rectangle rect = new Rectangle(0.0, 0.0);
+    @Test
+    public void mainTestFunction() {
+        try {
+            Main m = new Main();
+            Main.main(new String[]{"33", "10"});
+        } catch (Exception err) {
+            fail("Error");
+        }
     }
 }
